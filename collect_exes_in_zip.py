20b1ce4cc0e9ecb9c5411fc74a78c0c43d853d43
masonhyman-school <79164179+masonhyman-school@users.noreply.github.com>
2024-02-24 15:07:11 -0500

    Update collect_exes_in_zip.py

--- collect_exes_in_zip.py
+++ collect_exes_in_zip.py
@@ -29,7 +29,10 @@ def copy_to_zip(executables):
     with zipfile.ZipFile("executables.zip", "w") as zip_file:
         for exe in executables:
             hash_value = compute_hash(exe)
-            zip_file.write(exe, arcname=hash_value + ".exe")
+            try:
+                zip_file.write(exe, arcname=hash_value + ".exe")
+            except:
+                continue
 
 if __name__ == "__main__":
     directory = "C:\\Program Files"  # Change this to the directory you want to search for executables
