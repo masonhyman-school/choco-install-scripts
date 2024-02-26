d892921d0b6aea2971928a0fae81535f149557a2
masonhyman-school <79164179+masonhyman-school@users.noreply.github.com>
2024-02-26 09:39:56 -0500

    Update collect_exes_in_zip.py

--- collect_exes_in_zip.py
+++ collect_exes_in_zip.py
@@ -8,7 +8,7 @@ def get_executables(directory):
         for file in files:
             if file.endswith(".exe"):
                 executables.append(os.path.join(root, file))
-    print('Found: ' + str(len(executables) + ' exe files.'))
+    print('Found: ' + str(len(executables)) + ' exe files.')
     return executables
 
 def compute_hash(file_path):
@@ -27,20 +27,23 @@ def compute_hash(file_path):
 def copy_to_zip(executables, output_file):
     print('Zipping exe files, duplicates expected.')
     with zipfile.ZipFile(output_file, "w") as zip_file:
+       successful_count = 0
         for exe in executables:
             hash_value = compute_hash(exe)
             try:
                 zip_file.write(exe, arcname=hash_value + ".exe")
+                successful_count += 1
             except:
                 continue
+    return successful_count
 
 if __name__ == "__main__":
-    directory = input("Enter directory to search for exe files in (most likely C:\): ")  # Change this to the directory you want to search for executables
+    directory = input("Enter directory to search for exe files in (most likely C:\\): ")  # Change this to the directory you want to search for executables
     output_file = input("Enter the location where the zip file containing the exe files should be written to: ")
     executables = get_executables(directory)
     if executables:
-        copy_to_zip(executables, output_file)
-        print("Executable files copied to executables.zip")
+        num_copied = copy_to_zip(executables, output_file)
+        print(str(num_copied) + " executable files copied to " + output_file)
     else:
         print("No executable files found.")
 
